c340abccb4b65e66dd7a5fdf79fe0ef57a24d200
Jarppe <jarppe@metosin.fi>
2015-11-24 22:22:26 +0200

    Add favicon.

--- src/clj/backend/static.clj
+++ src/clj/backend/static.clj
@@ -8,13 +8,15 @@
             [backend.cache :as cache])
   (:import (org.apache.commons.codec.digest DigestUtils)))
 
-(defn with-version [resource-name]
-  (str resource-name (some-> resource-name
-                             (io/resource)
-                             (io/input-stream)
-                             (DigestUtils/sha256Hex)
-                             (subs 0 8)
-                             (as-> hash (str "?_=" hash)))))
+(defn with-version
+  ([resource-name] (with-version resource-name ""))
+  ([resource-name root]
+   (str resource-name (some-> (str root resource-name)
+                              (io/resource)
+                              (io/input-stream)
+                              (DigestUtils/sha256Hex)
+                              (subs 0 8)
+                              (as-> hash (str "?_=" hash))))))
 
 (def index-page
   (html
@@ -24,6 +26,8 @@
        [:meta {:charset "utf-8"}]
        [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge"}]
        [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+       [:link {:rel "icon" :type "image/x-icon" :href (with-version "/favicon.ico" "public")}]
+       [:link {:rel "shortcut icon" :type "image/x-icon" :href (with-version "/favicon.ico" "public")}]
        (include-css (with-version "css/main.css"))]
       [:body
        [:div#app
@@ -40,7 +44,13 @@
     (-> (ok index-page)
         (resp/content-type "text/html; charset=\"UTF-8\"")
         (cache/cache-control cache/no-cache)))
-
+  (GET* "/favicon.ico" []
+    :no-doc true
+    (-> "public/favicon.ico"
+        (io/resource)
+        (io/input-stream)
+        (ok)
+        (resp/content-type "image/x-icon")))
   (context* "/js" []
     (resources "" {:root "public/js"})
     (resources "" {:root "js"}))
