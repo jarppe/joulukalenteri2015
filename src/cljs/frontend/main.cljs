88666ee1b4741b1e725d8cd802a5ed1d148674a5
Jarppe <jarppe@metosin.fi>
2015-12-01 03:32:18 +0200

    User adjustable lang.

--- src/cljs/frontend/main.cljs
+++ src/cljs/frontend/main.cljs
@@ -6,21 +6,15 @@
             [frontend.hatch-pos :refer [hatch-positions]]
             [frontend.loc :as loc]))
 
-(def debug? false)
-
-(def terms (loc/terms js/window.lang))
-
 (def revealed-img "img/r.jpeg")
 (defn px [v] (str v "px"))
 
 (def active-hatches
-  (if debug?
-    24
-    (let [now (js/Date.)]
-      (cond
-        (> (.getYear now) 115) 24
-        (< (.getMonth now) 11) 0
-        :else (.getDate now)))))
+  (let [now (js/Date.)]
+    (cond
+      (> (.getYear now) 115) 24
+      (< (.getMonth now) 11) 0
+      :else (.getDate now))))
 
 (defn make-hatches []
   (map (fn [{:keys [x y x2 y2]} n can-open?]
@@ -35,12 +29,12 @@
        (concat (repeat active-hatches true)
                (repeat false))))
 
-(defn make-tooltip-message [n]
+(defn make-tooltip-message [terms n]
   (let [days-until (- n active-hatches)]
     (or (get-in terms [:until days-until])
         ((terms :until-more) days-until))))
 
-(defn active-hatch-component [{:keys [n x y w h]} opened?]
+(defn active-hatch-component [terms {:keys [n x y w h]} opened?]
   [:div.hatch.allowed {:class    (if @opened? "opened" "closed")
                        :style    {:left        (px x)
                                   :top         (px y)
@@ -54,8 +48,8 @@
                     :top      (px (* y -1))}
             :src   revealed-img}])])
 
-(defn inactive-hatch-component [{:keys [n x y w h]}]
-  (let [tooltip-message (make-tooltip-message n)
+(defn inactive-hatch-component [terms {:keys [n x y w h]}]
+  (let [tooltip-message (make-tooltip-message terms n)
         tooltip? (r/atom false)
         tooltip! (fn [v] (reset! tooltip? v))
         tooltip-task (atom nil)
@@ -82,29 +76,42 @@
                                 :top  (px (+ y h 10))}}
           [:p tooltip-message]])])))
 
-(defn hatch-component [{:keys [can-open?] :as hatch} opened?]
+(defn hatch-component [terms {:keys [can-open?] :as hatch} opened?]
   (if can-open?
-    [active-hatch-component hatch opened?]
-    [inactive-hatch-component hatch]))
+    [active-hatch-component terms hatch opened?]
+    [inactive-hatch-component terms hatch]))
+
+(defn flag [flag-lang lang]
+  (js/console.log "F:" flag-lang)
+  (js/console.log "L:" @lang)
+  (js/console.log "A:" (if (= flag-lang @lang) "active"))
+  [:a {:on-click (fn [_] (reset! lang flag-lang))}
+   [:img {:src (str "/img/" flag-lang ".png")
+          :class (if (= flag-lang @lang) "active")}]])
 
 (defn main-view []
-  (let [opened (local-storage (r/atom {}) :opened-2015)]
+  (let [opened (local-storage (r/atom {}) :opened-2015)
+        lang (r/atom (or js/window.lang "en"))]
     (fn []
-      [:div#app
-       [:header
-        [:h1 (terms :title)]
-        [:h2 (terms :help)]]
-       [:article
-        [:div#image-wrapper
-         (for [{n :n :as hatch} (make-hatches)]
-           ^{:key n} [hatch-component hatch (r/cursor opened [n])])]
-        [:img#main-image {:src "img/k.jpeg"}]]
-       [:footer
-        [:p (terms :art-copy)]
-        [:p
-         [:a {:href "https://github.com/jarppe/joulukalenteri2015" :target "_blank"} (terms :code)]
-         " "
-         (terms :code-copy)]]])))
+      (let [terms (get loc/terms @lang)]
+        [:div#app
+         [:div.lang
+          [flag "fi" lang]
+          [flag "en" lang]]
+         [:header
+          [:h1 (terms :title)]
+          [:h2 (terms :help)]]
+         [:article
+          [:div#image-wrapper
+           (for [{n :n :as hatch} (make-hatches)]
+             ^{:key n} [hatch-component terms hatch (r/cursor opened [n])])]
+          [:img#main-image {:src "img/k.jpeg"}]]
+         [:footer
+          [:p (terms :art-copy)]
+          [:p
+           [:a {:href "https://github.com/jarppe/joulukalenteri2015" :target "_blank"} (terms :code)]
+           " "
+           (terms :code-copy)]]]))))
 
 (defn init! []
   (r/render [main-view] (js/document.getElementById "app")))
